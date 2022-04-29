c34e133ac25e2a8e71bd390c11234cf496fa434b
wangxu <geolifestudy@gmail.com>
2022-04-29 14:58:30 +0800

    测试

---
+++ codepen/原生/showSelfInfo/info.js
@@ -0,0 +1,20 @@
+let Info = {
+  "type": "FeaturePerson",
+  "geometry": {
+    "type": "Point",
+    // https://www.w3school.com.cn/html/html5_geolocation.asp    计算显示距离
+    "coordinates": [125.6, 10.1]
+  },
+  "properties": {
+    "name": "itild",
+    "history":[
+      {
+        time:[20200101,20200102]
+      }
+    ]
+
+  }
+}
+
+
+
---
+++ codepen/原生/设备信息定位等/README.md
@@ -0,0 +1,6 @@
+
+## 设备信息
+window.navigator
+
+##     计算显示距离
+https://www.w3school.com.cn/html/html5_geolocation.asp 
