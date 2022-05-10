6bc78412c70f5a30dc4862a69260d88dabc44d94
wangxu <geolifestudy@gmail.com>
2022-05-10 17:34:12 +0800

    测试

---
+++ codepen/原生/转码utf8/template.js
@@ -0,0 +1,37 @@
+let testFunc = (data) => {
+
+
+  console.log('test:', str2utf8('一a'))
+  // Uint8Array(4) [228, 184, 128, 97,
+}
+
+
+let str2utf8 = window.TextEncoder ? function (str) {
+  let encoder = new TextEncoder('utf8');
+  return encoder.encode(str);
+} : function (str) {
+  // https://segmentfault.com/a/1190000021311212
+  let bytes = new Array();
+  let len, c;
+  len = str.length;
+  for (let i = 0; i < len; i++) {
+    c = str.charCodeAt(i);
+    if (c >= 0x010000 && c <= 0x10FFFF) {
+      bytes.push(((c >> 18) & 0x07) | 0xF0);
+      bytes.push(((c >> 12) & 0x3F) | 0x80);
+      bytes.push(((c >> 6) & 0x3F) | 0x80);
+      bytes.push((c & 0x3F) | 0x80);
+    } else if (c >= 0x000800 && c <= 0x00FFFF) {
+      bytes.push(((c >> 12) & 0x0F) | 0xE0);
+      bytes.push(((c >> 6) & 0x3F) | 0x80);
+      bytes.push((c & 0x3F) | 0x80);
+    } else if (c >= 0x000080 && c <= 0x0007FF) {
+      bytes.push(((c >> 6) & 0x1F) | 0xC0);
+      bytes.push((c & 0x3F) | 0x80);
+    } else {
+      bytes.push(c & 0xFF);
+    }
+  }
+  let encoderBytes = new Uint8Array(bytes);
+  return encoderBytes;
+}
