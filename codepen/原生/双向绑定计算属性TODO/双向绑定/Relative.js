7cb747ed2591abc88e2b3af656bf9fb2858f7223
wangxu <geolifestudy@gmail.com>
2022-06-09 17:44:08 +0800

    测试

---
+++ codepen/原生/双向绑定计算属性TODO/双向绑定/Relative.js
@@ -0,0 +1,26 @@
+/**
+ * 双向绑定计算属性
+ * @param {Object} obj
+ * https://blog.csdn.net/qweeryyioop/article/details/121760685
+ */
+class Relative {
+  /**
+   * 
+   * @param {Object} obj  待计算对象
+   * @param  {...Function} fns  可变参数，可以传入多个计算属性
+   */
+  constructor(obj,...fns) {
+    this.obj = obj;
+    this.fns = fns;
+    this.call()
+  }
+
+  update() {
+    
+  }
+  call() {
+    this.fns.forEach(fn => {
+      fn.call(this.obj);
+    });
+  }
+}
---
+++ codepen/原生/双向绑定计算属性TODO/双向绑定/template.js
@@ -0,0 +1,3 @@
+let testFunc = (data) =>{
+  console.log('test:',data)
+}
